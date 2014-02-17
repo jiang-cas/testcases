774c31b992045cfb865abb3a4dc947437449b5ad
jiang-cas <jiang.cas@gmail.com>
2014-02-17 15:22:14 +0800

    first commit

---
+++ barrier-test.c
@@ -0,0 +1,45 @@
+#include "sthread.h"
+#include <stdio.h>
+
+#define N 2
+
+int *a;
+sthread_barrier_t barrier;
+
+void *func(void *args)
+{
+	int i, k;
+	sthread_t thread;
+	thread = sthread_self();
+	i = thread.tid;
+	for(k=0;k<10;k++) {
+		a[i] = i + 10;
+		sthread_barrier_wait(&barrier);
+	}
+	sthread_exit(NULL);
+	return NULL;
+}
+
+int main()
+{
+	int i;
+	sthread_t thread[N];
+	sthread_barrier_init(&barrier, NULL, N);
+	a = (int *)mvshared_malloc(sizeof(int) * N);
+
+	for(i=0;i<N;i++)
+		sthread_create(&thread[i], NULL, func, NULL);
+
+	sthread_main_wait(N);
+
+//	sthread_barrier_wait(&barrier);
+
+	for(i=0;i<N;i++)
+		printf("%d ", a[i]);
+	printf("\n");
+	for(i=0;i<N;i++)
+		sthread_join(thread[i], NULL);
+
+	mvshared_free(a);
+	return 0;
+}
---
+++ mutex-test.c
@@ -0,0 +1,45 @@
+#include "sthread.h"
+#include <stdio.h>
+
+#define N 10
+
+int *a;
+sthread_mutex_t mutex;
+
+void *func(void *args)
+{
+	int i, j;
+	sthread_t thread;
+	thread = sthread_self();
+	i = thread.tid;
+	sthread_mutex_lock(&mutex);
+	for(j=0;j<N;j++)
+		a[j] = i + j + 10;
+	sthread_mutex_unlock(&mutex);
+	sthread_exit(NULL);
+	return NULL;
+}
+
+int main()
+{
+	int i;
+	sthread_t thread[N];
+	sthread_mutex_init(&mutex, NULL);
+	a = (int *)mvshared_malloc(sizeof(int) * N);
+
+	for(i=0;i<N;i++)
+		sthread_create(&thread[i], NULL, func, NULL);
+
+	sthread_main_wait(N);
+
+	for(i=0;i<N;i++)
+		sthread_join(thread[i], NULL);
+	
+	for(i=0;i<N;i++)
+		printf("%d ", a[i]);
+	printf("\n");
+	
+	sthread_mutex_destroy(&mutex);
+	mvshared_free(a);
+	return 0;
+}
