d7057ef28f6cdab1a800a1c77e468b816ff07dbb
jiangpeng <jiangpeng@athene.(none)>
2014-02-17 16:40:32 +0800

    bug in cond

---
+++ cond-test.c
@@ -0,0 +1,99 @@
+#include "sthread.h"
+#include <stdio.h> 
+ 
+#define NUM_THREADS  3 
+#define TCOUNT 10 
+#define COUNT_LIMIT 12 
+ 
+int     *count; 
+int     thread_ids[3] = {0,1,2}; 
+sthread_mutex_t count_mutex; 
+sthread_cond_t count_threshold_cv; 
+ 
+void *inc_count(void *idp)  
+{ 
+  int j,i; 
+  double result=0.0; 
+  int *my_id = (int *)idp; 
+ 
+  for (i=0; i<TCOUNT; i++) { 
+    sthread_mutex_lock(&count_mutex); 
+    (*count)++; 
+    printf("inc_count(): thread %d, count = %d, locking mutex\n", *my_id, *count); 
+ 
+    /*  
+    Check the value of count and signal waiting thread when condition is 
+    reached.  Note that this occurs while mutex is locked.  
+    */ 
+    if (*count == COUNT_LIMIT) { 
+      sthread_cond_signal(&count_threshold_cv); 
+      printf("inc_count(): thread %d, count = %d  Threshold reached.\n",  
+             *my_id, *count); 
+      } 
+    printf("inc_count(): thread %d, count = %d, unlocking mutex\n",  
+           *my_id, *count); 
+    sthread_mutex_unlock(&count_mutex); 
+ 
+    /* Do some work so threads can alternate on mutex lock */ 
+    for (j=0; j<1000; j++) 
+      result = result + j; 
+    } 
+	sthread_exit(NULL); 
+} 
+ 
+void *watch_count(void *idp)  
+{ 
+  int *my_id = (int *)idp; 
+ 
+  printf("Starting watch_count(): thread %d\n", *my_id); 
+ 
+  /* 
+  Lock mutex and wait for signal.  Note that the pthread_cond_wait  
+  routine will automatically and atomically unlock mutex while it waits.  
+  Also, note that if COUNT_LIMIT is reached before this routine is run by 
+  the waiting thread, the loop will be skipped to prevent pthread_cond_wait 
+  from never returning.  
+  */ 
+  sthread_mutex_lock(&count_mutex); 
+  if (*count<COUNT_LIMIT) { 
+    sthread_cond_wait(&count_threshold_cv, &count_mutex); 
+    printf("watch_count(): thread %d Condition signal received.\n", *my_id); 
+    } 
+  sthread_mutex_unlock(&count_mutex); 
+  sthread_exit(NULL); 
+} 
+ 
+int main (int argc, char *argv[]) 
+{ 
+  int i, rc; 
+  sthread_t threads[3]; 
+
+	count = (int *)mvshared_malloc(sizeof(int));
+	*count = 0;
+//  pthread_attr_t attr; 
+ 
+  /* Initialize mutex and condition variable objects */ 
+  sthread_mutex_init(&count_mutex, NULL); 
+  sthread_cond_init (&count_threshold_cv, NULL); 
+ 
+  /* For portability, explicitly create threads in a joinable state */ 
+ // pthread_attr_init(&attr); 
+ // pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); 
+  sthread_create(&threads[0], NULL, inc_count, (void *)&thread_ids[0]); 
+  sthread_create(&threads[1], NULL, inc_count, (void *)&thread_ids[1]); 
+  sthread_create(&threads[2], NULL, watch_count, (void *)&thread_ids[2]); 
+	
+sthread_main_wait(3);
+ 
+  /* Wait for all threads to complete */ 
+  for (i=0; i<NUM_THREADS; i++) { 
+    sthread_join(threads[i], NULL); 
+  } 
+  printf ("Main(): Waited on %d  threads. Done.\n", NUM_THREADS); 
+ 
+  /* Clean up and exit */ 
+ // pthread_attr_destroy(&attr); 
+  sthread_mutex_destroy(&count_mutex); 
+  sthread_cond_destroy(&count_threshold_cv); 
+  sthread_exit(NULL); 
+} 
---
+++ sthread.h
@@ -0,0 +1,128 @@
+#ifndef STHREAD_H
+#define STHREAD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef unsigned long size_t;
+
+#define MAXTHREADS 32
+
+#define mspace void *
+
+	void *mvprivate_malloc(size_t bytes);
+
+	void mvprivate_free(void *mem);
+
+	void *mvshared_malloc(size_t bytes);
+
+	void mvshared_free(void *mem);
+
+	struct mutex_struct
+	{
+		int lock[MAXTHREADS];
+		int inited;
+	};
+
+	typedef struct sthread_mutex_t
+	{
+		struct mutex_struct *mutex;
+	} sthread_mutex_t;
+
+	typedef struct sthread_mutexattr_t
+	{} sthread_mutexattr_t;
+
+	int sthread_mutex_init(sthread_mutex_t *mutex, const sthread_mutexattr_t *attr);
+
+	int sthread_mutex_destroy(sthread_mutex_t *mutex);
+
+	int sthread_mutex_lock(sthread_mutex_t *mutex);
+
+	int sthread_mutex_unlock(sthread_mutex_t *mutex);
+
+	struct barrier_struct 
+	{
+		int total;
+		int num;
+		int sema;
+	};
+
+	typedef struct sthread_barrier_t 
+	{
+		struct barrier_struct *barrier;
+	} sthread_barrier_t;
+
+	typedef struct sthread_barrierattr_t {} sthread_barrierattr_t;
+
+	int sthread_barrier_init(sthread_barrier_t *barrier, const sthread_barrierattr_t *attr, unsigned int count);
+
+	int sthread_barrier_wait(sthread_barrier_t *barrier);
+
+	int sthread_barrier_destroy(sthread_barrier_t *barrier);
+
+
+	struct cond_struct {
+			int locks;
+	};
+
+	typedef struct sthread_cond_t
+	{
+			struct cond_struct *cond;
+	} sthread_cond_t;
+
+	typedef struct sthread_condattr_t
+	{
+	} sthread_condattr_t;
+
+	int sthread_cond_init(sthread_cond_t *cond, sthread_condattr_t *cond_attr);
+
+	int sthread_cond_signal(sthread_cond_t *cond);
+
+	int sthread_cond_broadcast(sthread_cond_t *cond);
+
+	int sthread_cond_wait(sthread_cond_t *cond, sthread_mutex_t *mutex);
+
+	int sthread_cond_timewait(sthread_cond_t *cond, sthread_mutex_t *mutex, const struct timespec *abstime);
+
+	int sthread_cond_destroy(sthread_cond_t *cond);
+
+	struct heap_struct
+	{
+			void *privatebase;
+			mspace privatemsp;
+			void *sharedbase;
+			mspace sharedmsp;
+	};
+
+	typedef struct sthread_t
+	{
+			unsigned int tid;
+			void *retval;
+			int pid;
+			struct heap_struct heap;
+			int state;
+			int lock1;
+			int lock2;
+			int joinlock;
+			struct mutex_struct *mutex;
+			struct barrier_struct *barrier;
+			int leaved;
+
+	} sthread_t;
+
+	typedef struct sthread_attr_t 
+	{
+			unsigned int stack_size;
+	} sthread_attr_t;
+
+	__attribute__((constructor)) void init();
+	int sthread_create(sthread_t *newthread, sthread_attr_t *attr, void *(*func)(void *), void *args);
+	sthread_t sthread_self(void);
+	void sthread_exit(void *value);
+	int sthread_join(sthread_t thread, void **thread_return);
+	void sthread_main_wait(int n);
+#ifdef __cplusplus
+}
+#endif
+#endif
